10cf6c1906000bc0b12e97ccf31baac8be9fd3c8
chIvonnebelen <ivonebelenona@gmail.com>
2023-01-18 01:11:51 -0300

    files

---
+++ src/main/java/com/example/presentationdeploy/PresentationDeployApplication.java
@@ -0,0 +1,14 @@
+package com.example.presentationdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentationDeployApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PresentationDeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/presentationdeploy/PresentationDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.presentationdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresentationDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
